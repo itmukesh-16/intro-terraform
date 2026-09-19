65526363a86bdd4c27d8a36683ac4562af468d94
root <root@ip-10-0-0-48.ec2.internal>
2026-09-19 10:20:36 +0000

    ec2 web-instance created by dev-2

--- terraform-state-locking/main.tf
+++ terraform-state-locking/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0fef201115eefe936" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public.id
 
