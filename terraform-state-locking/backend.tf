54601cd95b68927f1616137b2eef73265825be2a
Mukesh <109750504+itmukesh-16@users.noreply.github.com>
2026-09-19 15:01:45 +0530

    vpc+igw+rt+subnet created and backend.tf uploaded into s3 bucket

---
+++ terraform-state-locking/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+    backend "s3" {
+        bucket         = "terraform-statefile-943553143518"
+        key            = "terraform.tfstate"
+        region         = "us-east-1"
+    }
+}
---
+++ terraform-state-locking/main.tf
@@ -0,0 +1,43 @@
+#vpc creation  
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags={
+    Name="main-vpc"
+  }
+}
+
+#internet gateway creation and attach to vpc
+resource "aws_internet_gateway" "igw"{
+    vpc_id =aws_vpc.main.id
+
+    tags={
+        Name="igw"
+    }
+}
+
+#public subnet creation 
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
